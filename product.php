c988a622a4e42dc38373ca2d7d46956891202e40
mina-rb <143052060+mina-rb@users.noreply.github.com>
2023-11-09 10:32:54 -0500

    Add files via upload

---
+++ product.php
@@ -0,0 +1,23 @@
+<?php
+include "db_config.php";
+include "WarehouseDatabase.php";
+try {
+  // Connect to the database
+  $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  // Fetch data from the product table
+  $sql = "SELECT productId, productName FROM product";
+  $stmt = $conn->prepare($sql);
+  $stmt->execute();
+  $productOptions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+  // Return the product options as JSON response
+  header('Content-Type: application/json');
+  echo json_encode($productOptions);
+
+} catch(PDOException $e) {
+  // Failed to connect or retrieve data, handle the error
+  echo "Error: " . $e->getMessage();
+}
+?>
