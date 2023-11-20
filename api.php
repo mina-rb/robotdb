0ddc7eb0c12651f9cef1064d41710c8aeeb7e863
mina-rb <143052060+mina-rb@users.noreply.github.com>
2023-11-20 15:45:48 -0500

    Update api.php

--- api.php
+++ api.php
@@ -1,7 +1,8 @@
 <?php
 include "db_config.php";
-include "classes/WarehouseDatabase.php";
-$sql = "SELECT productId,productName FROM product";
-$result = WarehouseDatabase::getDataFromSQL($sql);
-print_r($result);
+include "WarehouseDatabase.php";
+try {
+  // Connect to the database
+  $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
